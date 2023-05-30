753fec76a9b9e1fae80f65b43f25b0f5c93c5128
julydeath <manojkarajada.mk@gmail.com>
2023-05-30 11:28:22 +0530

    chore ; tailwind installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return <div className="text-red-500">Hello</div>;
 }
 
 export default App;
